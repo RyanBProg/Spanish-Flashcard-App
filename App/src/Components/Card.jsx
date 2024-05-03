88604f8b6789d0fae0e456c6342421a276ba9a7e
Ryan <ryanbprog@gmail.com>
2024-05-03 15:53:06 +0800

    Card flip animation created -card flip animation complete for onClick -card swap animation started and set to :hover for testing

--- App/src/Components/Card.jsx
+++ App/src/Components/Card.jsx
@@ -5,15 +5,27 @@ const Card = ({ state, categoryData }) => {
   const currentWord = categoryData[state.categoryIndex].words[state.wordIndex];
 
   return (
-    <div className="card-container">
-      <h3 className="card-word-txt">
-        {cardFlip ? currentWord.engTranslation : currentWord.spanWord}
-      </h3>
-      <button className="reveal-btn" onClick={() => setCardFlip(!cardFlip)}>
-        Reveal Answer
-      </button>
+    <div className="container">
+      <div
+        className={`flashcard-container ${cardFlip ? "flashcard-flipped" : ""}`}
+        onClick={() => setCardFlip(!cardFlip)}>
+        <div className="flashcard flashcard-front">
+          <h3 className="card-word-txt">{currentWord.spanWord}</h3>
+          <p className="reveal-txt">Click to flip card</p>
+        </div>
+        <div className="flashcard flashcard-back">
+          <h3 className="card-word-txt">{currentWord.engTranslation}</h3>
+          <p className="reveal-txt">Click to flip card</p>
+        </div>
+      </div>
+      <div className="flashcard flashcard-behind">
+        <h3 className="card-word-txt">{currentWord.spanWord}</h3>
+        <p className="reveal-txt">Click to flip card</p>
+      </div>
     </div>
   );
 };
 
 export default Card;
+
+// flip back card when changing to next card
