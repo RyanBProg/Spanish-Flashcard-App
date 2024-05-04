a413b26d67678e405a7d793732a986021e2514b9
Ryan <ryanbprog@gmail.com>
2024-05-04 21:26:48 +0800

    Contorls styling -controls component styling complete -last card in stack has an image

--- App/src/Components/Card.jsx
+++ App/src/Components/Card.jsx
@@ -1,15 +1,28 @@
 import React from "react";
 import spainFlagIcon from "../assets/images/spain-flag-icon.png";
 import spainMapIcon from "../assets/images/spain-map-icon.png";
+import sagradaIcon from "../assets/images/sagrada-familia-icon.png";
 
 const BackCard = ({ categoryData, state }) => {
   if (state.wordIndex === state.deckSize - 1) {
-    return <h3 className="card-word-txt">Last Card</h3>;
+    return (
+      <img src={sagradaIcon} alt="sagrada familia icon" className="card-icon" />
+    );
   } else {
     return (
-      <h3 className="card-word-txt">
-        {categoryData[state.categoryIndex].words[state.wordIndex + 1].spanWord}
-      </h3>
+      <>
+        <img src={spainFlagIcon} alt="spain flag icon" className="card-icon" />
+        <p className="card-number">
+          {state.wordIndex + 2}/<span>{state.deckSize}</span>
+        </p>
+        <h3 className="card-word-txt">
+          {
+            categoryData[state.categoryIndex].words[state.wordIndex + 1]
+              .spanWord
+          }
+        </h3>
+        <p className="reveal-txt">Click to flip card</p>
+      </>
     );
   }
 };
@@ -27,20 +40,28 @@ const Card = ({ state, categoryData, dispatch, animate }) => {
           dispatch({ type: "card_flipped" });
         }}>
         <div className="flashcard">
-          <img src={spainFlagIcon} alt="" className="card-icon" />
+          <img
+            src={spainFlagIcon}
+            alt="spain flag icon"
+            className="card-icon"
+          />
+          <p className="card-number">
+            {state.wordIndex + 1}/<span>{state.deckSize}</span>
+          </p>
           <h3 className="card-word-txt">{currentWord.spanWord}</h3>
           <p className="reveal-txt">Click to flip card</p>
         </div>
         <div className={`flashcard flashcard-back`}>
-          <img src={spainMapIcon} alt="" className="card-icon" />
+          <img src={spainMapIcon} alt="spain map icon" className="card-icon" />
+          <p className="card-number">
+            {state.wordIndex + 1}/<span>{state.deckSize}</span>
+          </p>
           <h3 className="card-word-txt">{currentWord.engTranslation}</h3>
           <p className="reveal-txt">Click to flip card</p>
         </div>
       </div>
       <div className="flashcard flashcard-behind">
-        <img src={spainFlagIcon} alt="" className="card-icon" />
         <BackCard categoryData={categoryData} state={state} />
-        <p className="reveal-txt">Click to flip card</p>
       </div>
     </div>
   );
--- App/src/Components/Controls.jsx
+++ App/src/Components/Controls.jsx
@@ -1,27 +1,32 @@
 import React from "react";
+import tickIcon from "../assets/images/tick-icon.png";
+import crossIcon from "../assets/images/cross-icon.png";
 
 const Controls = ({ state, dispatch, handleCardSwap }) => {
   return (
-    <div className="controls-container">
-      <button
-        disabled={state.gameFinished}
-        className="controls-btn correct-btn"
-        onClick={() => {
-          handleCardSwap("answered_correct");
-        }}>
-        Correct
-      </button>
-      <button
-        disabled={state.gameFinished}
-        className="controls-btn incorrect-btn"
-        onClick={() => {
-          handleCardSwap("answered_incorrect");
-        }}>
-        Incorrect
-      </button>
-      <p className="counters">Score: {state.score}</p>
-      <p className="counters">
-        Card {state.wordIndex + 1} out of {state.deckSize}
+    <div>
+      <div className="controls-container">
+        <button
+          disabled={state.gameFinished}
+          className="controls-btn correct-btn"
+          onClick={() => {
+            handleCardSwap("answered_correct");
+          }}>
+          <span>Correct</span>
+          <img src={tickIcon} alt="tick icon" />
+        </button>
+        <button
+          disabled={state.gameFinished}
+          className="controls-btn incorrect-btn"
+          onClick={() => {
+            handleCardSwap("answered_incorrect");
+          }}>
+          <span>Incorrect</span>
+          <img src={crossIcon} alt="cross icon" />
+        </button>
+      </div>
+      <p className="counter">
+        Score: <span>{state.score}</span>
       </p>
     </div>
   );
